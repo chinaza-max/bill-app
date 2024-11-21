0f0e251736adda15576ad1104a9b01e7e65f9f13
chinaza-max <mosesogbonna68@gmail.com>
2024-11-21 01:17:42 +0100

    test

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,7 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-
+/*
 export const viewport = {
   themeColor: '#000000',
   viewportFit: 'cover',
@@ -26,16 +26,55 @@ export const viewport = {
   minimumScale: 1,
   maximumScale: 1,
   userScalable: false,
-}
+}*/
 
+
+
+const APP_NAME = "Fintread";
+const APP_DEFAULT_TITLE = "My Awesome PWA App";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "Best PWA app in the world!";
 
 
 export const metadata = {
-  title: 'Fintread',
-  description: 'Your app description',
-  manifest: '/manifest.json',
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 };
 
+export const viewport = {
+  themeColor: "#FFFFFF",
+};
 
 export default function RootLayout({ children }) {
   return (
