2c60c728c2b07774e7558d02b1baf047ee06b331
macBookpro <macbookpro@chinaza.local>
2026-02-15 03:47:59 +0100

    update

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://fidopoint.onrender.com/api/v1",
   headers: {
     "Content-Type": "application/json", // default content-type for the API requests
   },
