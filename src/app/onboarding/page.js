c76f127b56fbe39f2e34d70b7ffaa368053b6455
macBookpro <macbookpro@chinaza.local>
2026-03-20 17:33:21 +0100

    update

--- src/app/onboarding/page.js
+++ src/app/onboarding/page.js
@@ -118,7 +118,7 @@ export default function Home() {
 
   return (
     <>
-      {isOverlayVisible2 ? (
+      {false ? (
         <Overlay isVisible={isOverlayVisible} />
       ) : (
         <div
