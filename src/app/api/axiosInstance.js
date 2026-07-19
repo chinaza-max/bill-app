fc33a8a05ad5da979edf3382248fea5c4352756c
macBookpro <macbookpro@chinaza.local>
2026-07-20 00:52:47 +0100

    update

--- src/app/api/axiosInstance.js
+++ src/app/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api/v1",
-  headers: {
+  baseURL: process.env.NEXT_PUBLIC_API_URL ||"https://bill-bolt.onrender.com/api/v1" ||"http://localhost:5000/api/v1",
+  headers: { 
     "Content-Type": "application/json",
   },
   withCredentials: true, // This ensures cookies are sent with each request
